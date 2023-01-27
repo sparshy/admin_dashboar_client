b49a0d3f1e1da17d76cec3cbbe4bbb63323a947f
thenamannanda <naman.3727@gmail.com>
2023-01-28 03:31:19 +0530

    added sidebar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,56 +1,67 @@
-import React from 'react';
-import { LightModeOutlined, DarkModeOutlined, Search, Menu as MenuIcon, SettingsOutlined } from '@mui/icons-material';
-import { useDispatch } from 'react-redux';
-import { AppBar, IconButton, InputBase, Toolbar, useTheme } from '@mui/material';
-import FlexBetween from './FlexBetween';
-import { setMode } from 'state';
+import React from "react";
+import {
+  LightModeOutlined,
+  DarkModeOutlined,
+  Search,
+  Menu as MenuIcon,
+  SettingsOutlined,
+} from "@mui/icons-material";
+import { useDispatch } from "react-redux";
+import {
+  AppBar,
+  IconButton,
+  InputBase,
+  Toolbar,
+  useTheme,
+} from "@mui/material";
+import FlexBetween from "./FlexBetween";
+import { setMode } from "state";
 
-
-
-
-
-const Navbar = () => {
-    const dispatch= useDispatch();
-    const theme= useTheme();
+const Navbar = ({ isSidebarOpen, setIsSidebarOpen }) => {
+  const dispatch = useDispatch();
+  const theme = useTheme();
   return (
-   <AppBar
-   sx={{
-    position: "static",
-    background: "none",
-    boxShadow: "none",
-   }}>
-        <Toolbar sx={{justifyContent: "space-between"}}>
-            {/**LEFT SIDE */}
-            <FlexBetween>
-                <IconButton onClick={ () => console.log("open/close sidebar")}>
-                    <MenuIcon/>
-                </IconButton>
-                <FlexBetween
-                    backgroundColor={theme.palette.background.alt}
-                    borderRadius="9px"
-                    gap="3rem"
-                    p="0.1 rem 1.5rem"
-                >
-                    <InputBase placeholder='Search...'/>
-                    <IconButton>
-                        <Search/>
-                    </IconButton>
-                </FlexBetween>
-            </FlexBetween>
-            {/**RIGHT SIDE */}
-            <FlexBetween gap="1.5rem">
-                <IconButton onClick={() => dispatch(setMode())}>
-                   { theme.palette.mode==='dark' ? (
-                   <DarkModeOutlined sx={{fontSize:"25px"}}   />): (
-                   <LightModeOutlined sx={{fontSize:"25px"}}  />)}
-                </IconButton>
-                <IconButton>
-                    <SettingsOutlined sx={{fontSize:"25px"}}  />
-                </IconButton>
-            </FlexBetween>
-        </Toolbar>
-   </AppBar>
-  )
-}
+    <AppBar
+      sx={{
+        position: "static",
+        background: "none",
+        boxShadow: "none",
+      }}
+    >
+      <Toolbar sx={{ justifyContent: "space-between" }}>
+        {/**LEFT SIDE */}
+        <FlexBetween>
+          <IconButton onClick={() => setIsSidebarOpen(!isSidebarOpen)}>
+            <MenuIcon />
+          </IconButton>
+          <FlexBetween
+            backgroundColor={theme.palette.background.alt}
+            borderRadius="9px"
+            gap="3rem"
+            p="0.1 rem 1.5rem"
+          >
+            <InputBase placeholder="Search..." />
+            <IconButton>
+              <Search />
+            </IconButton>
+          </FlexBetween>
+        </FlexBetween>
+        {/**RIGHT SIDE */}
+        <FlexBetween gap="1.5rem">
+          <IconButton onClick={() => dispatch(setMode())}>
+            {theme.palette.mode === "dark" ? (
+              <DarkModeOutlined sx={{ fontSize: "25px" }} />
+            ) : (
+              <LightModeOutlined sx={{ fontSize: "25px" }} />
+            )}
+          </IconButton>
+          <IconButton>
+            <SettingsOutlined sx={{ fontSize: "25px" }} />
+          </IconButton>
+        </FlexBetween>
+      </Toolbar>
+    </AppBar>
+  );
+};
 
-export default Navbar
+export default Navbar;
