230794336169b033e50e61879413863dc0a67769
thenamannanda <naman.3727@gmail.com>
2023-01-30 15:18:48 +0530

    added transaction data grid

--- src/App.js
+++ src/App.js
@@ -7,27 +7,28 @@ import { themeSettings } from "theme";
 import Dashboard from "scenes/dashboard";
 import Layout from "scenes/layout";
 import Products from "scenes/products";
-import Customers from "scenes/customers"
+import Customers from "scenes/customers";
+import Transactions from "scenes/transactions";
 
 function App() {
-  const mode= useSelector((state) => state.global.mode);
+  const mode = useSelector((state) => state.global.mode);
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
 
   return (
     <div className="app">
       <BrowserRouter>
-      
-      <ThemeProvider theme={theme}>
-        <CssBaseline/>
-        <Routes>
-          <Route element = {<Layout />}>
-            <Route path="/" element={<Navigate to ="/dashboard" replace />}/>
-            <Route path="/dashboard" element= {<Dashboard/>}/>
-            <Route path="/products" element={<Products/>} />
-            <Route path="/customers" element={<Customers/>} />
-          </Route>
-        </Routes>
-      </ThemeProvider>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route element={<Layout />}>
+              <Route path="/" element={<Navigate to="/dashboard" replace />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/products" element={<Products />} />
+              <Route path="/customers" element={<Customers />} />
+              <Route path="/transactions" element={<Transactions />} />
+            </Route>
+          </Routes>
+        </ThemeProvider>
       </BrowserRouter>
     </div>
   );
